9624590d1ec5feabf23acdaf11985639e4d7e6c4
Hugo Sanchez <hugo.sago30@gmail.com>
2024-05-28 10:55:53 -0600

    registro del usuario

--- Front-end/front-gimnasio/src/components/Login.vue
+++ Front-end/front-gimnasio/src/components/Login.vue
@@ -10,7 +10,7 @@
             </div>
             <div class="mt-12 flex flex-col items-center">
                 <h1 class="text-2xl xl:text-3xl font-extrabold">
-                    Sign up
+                    Iniar Sesion
                 </h1>
                 <div class="w-full flex-1 mt-8">
                     <div class="flex flex-col items-center">
@@ -33,7 +33,7 @@
                                 </svg>
                             </div>
                             <span class="ml-4">
-                                Sign Up with Google
+                                Continuar con Google
                             </span>
                         </button>
 
@@ -46,7 +46,7 @@
                                 </svg>
                             </div>
                             <span class="ml-4">
-                                Sign Up with GitHub
+                                Continuar con GitHub
                             </span>
                         </button>
                     </div>
@@ -54,17 +54,17 @@
                     <div class="my-12 border-b text-center">
                         <div
                             class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                            Or sign up with e-mail
+                            o continuar con correo
                         </div>
                     </div>
 
                     <div class="mx-auto max-w-xs">
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                            type="email" placeholder="Email" />
+                            type="email" placeholder="Correo" />
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="password" placeholder="Password" />
+                            type="password" placeholder="Contraseña" />
                         <button
                             class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                             <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
@@ -74,17 +74,17 @@
                                 <path d="M20 8v6M23 11h-6" />
                             </svg>
                             <span class="ml-3">
-                                Sign Up
+                                Registrar
                             </span>
                         </button>
                         <p class="mt-6 text-xs text-gray-600 text-center">
-                            I agree to abide by templatana's
+                            Estoy de acuerdo con BullsGym y sus
                             <a href="#" class="border-b border-gray-500 border-dotted">
-                                Terms of Service
+                                Termino de Servicio
                             </a>
-                            and its
+                            y su
                             <a href="#" class="border-b border-gray-500 border-dotted">
-                                Privacy Policy
+                                Politica de Privacidad
                             </a>
                         </p>
                     </div>
---
+++ Front-end/front-gimnasio/src/components/RegisterUser.vue
@@ -0,0 +1,83 @@
+<template>
+ 
+    <!-- source:https://codepen.io/owaiswiz/pen/jOPvEPB -->
+<div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+    <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+            <div>
+                <img src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
+                    class="w-32 mx-auto" />
+            </div>
+            <div class="mt-12 flex flex-col items-center">
+                <h1 class="text-2xl xl:text-3xl font-extrabold">
+                        Registrar
+                </h1>
+                <div class="w-full flex-1 mt-8">
+                    <div class="flex flex-col items-center">
+                        
+                    </div>
+
+                    
+
+                    <div class="mx-auto max-w-xs">
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                            type="nombre" placeholder="Nombre" />
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="text" placeholder="Pimer Apellido" />
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="text" placeholder="Segundo Apellido" />
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="text" placeholder="Titulo de cortesia" />
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha Naciemiento</label>
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="date" placeholder="Fecha de Nacimiento" />
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tu foto</label>
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="file" placeholder="Foto" />
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="text" placeholder="Genero" />
+
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                            type="text" placeholder="Sangre tipo" />
+                        <button
+                            class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                            <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                                <circle cx="8.5" cy="7" r="4" />
+                                <path d="M20 8v6M23 11h-6" />
+                            </svg>
+                            <span class="ml-3">
+                                Registrar
+                            </span>
+                        </button>
+                        <p class="mt-6 text-xs text-gray-600 text-center">
+                            Estoy de acuerdo con BullsGym y sus
+                            <a href="#" class="border-b border-gray-500 border-dotted">
+                                Termino de Servicio
+                            </a>
+                            y su
+                            <a href="#" class="border-b border-gray-500 border-dotted">
+                                Politica de Privacidad
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
+            <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+                style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');">
+            </div>
+        </div>
+    </div>
+</div>
+</template>
