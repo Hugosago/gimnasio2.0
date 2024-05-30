ae894227e4f2475be764cba454fc9a03971cad04
Hugo <hugo.sago30@gmail.com>
2024-05-30 11:53:28 -0600

    menu

--- Front-end/front-gimnasio/src/App.vue
+++ Front-end/front-gimnasio/src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
   <RouterView />
 </template>
 
+<style lang="scss">
+@import 'styles/layout.scss';
+@import 'styles/menu-toggle-btn.scss';
+@import 'styles/menu.scss';
+@import 'styles/content-overlay.scss';
+@import 'styles/media-queries.scss';
+</style>
--- Front-end/front-gimnasio/src/router/index.js
+++ Front-end/front-gimnasio/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../components/Login.vue'
 import RegisterUser from '../components/RegisterUser.vue'
+import Menu from '../components/Menu.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,19 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterUser
+    },
+    {
+      path: '/meu',
+      name: 'menu',
+      component: Menu
+    },
+    {
+      path: '/page/:sectionSlug',
+      name: 'dynamicContent',
+      // route level code-splitting
+      // this generates a separate chunk (dynamicContent.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: dynamicContent" */ '../views/DynamicContent.vue')
     }
   ]
 })
